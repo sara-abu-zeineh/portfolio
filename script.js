be146e403880372153e4002f37622c7f58fd02c7
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-07-31 02:33:56 +0300

    #15 - Modify navigation bar behavior and icon

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const hiddenBarIcon = document.getElementById('hidden-bar');
 const dropDownMenu = document.getElementById('drop-down');
+const hiddenBarIcon = document.getElementById('hidden-bar');
 
-hiddenBarIcon.onclick = function(){
-    dropDownMenu.classList.toggle("open");
+hiddenBarIcon.addEventListener('click', () => {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains('open');
+  hiddenBarIcon.classList = !isOpen ? 'fa-solid fa-bars' : 'fa-solid fa-xmark';
+});
+
+function closeNavBar() {
+//   dropDownMenu.classList.remove('open');
+//   hiddenBarIcon.classList = 'fa-solid fa-bars';
+  console.log("sara");
 }
